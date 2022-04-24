f1bdd30a310dd40ecbfc597668e1f9fb52399d65
bjerke2000 <90384750+bjerke2000@users.noreply.github.com>
2022-04-24 20:32:23 +0200

    Logikk for registrering av brukere

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from importlib.resources import path
 import pickle
 from pydoc import pathdirs
+from tokenize import String
 from types import NoneType
+from wsgiref.validate import validator
+from click import confirm
 from flask import Flask, redirect, render_template, flash, url_for, session
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, FileField, DateField
+from wtforms import StringField, PasswordField, SubmitField, FileField, DateField, SelectMultipleField
 from wtforms.validators import DataRequired, EqualTo, Length
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -16,7 +19,7 @@ import os
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "påskerally"
+app.config["SECRET_KEY"] = "esketit"
 app.config[
     "SQLALCHEMY_DATABASE_URI"
 ] = "mysql+pymysql://stud_v22_didriksenchr:R275uX1WYcttAKhb@kark.uit.no/stud_v22_didriksenchr"
@@ -24,15 +27,51 @@ app.config[
 UPLOAD_FOLDER = 'content'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
+# Login
+#login_manager = LoginManager()
+#login_manager.init_app(app)
+#login_manager.login_view='login'
+#login_manager.login_message = 'User needs to be logged in to view this poop!'
+#login_manager.login_message_category = 'error'
+
+class RegisterForm(FlaskForm):
+    name = StringField(
+        "Name",
+        validators=[DataRequired(), Length(max=150)],
+        render_kw={"autofocus":True, "placeholder": "name"},
+    )
+    email = StringField(
+        "Email",
+        validators=[DataRequired(), Length(max=150)],
+        render_kw={"placeholder":"Email"},
+    )
+    password = PasswordField(
+        "Password",
+        validators=[DataRequired(), EqualTo("password_confirm", "Passwords do not match")],
+        render_kw={"placeholder":"Password"}
+    )
+    password_confirm = PasswordField(
+        "Confirm password",
+        validators=[DataRequired()],
+        render_kw={"placeholder":"Confirm password"}
+    )
+    groups = SelectMultipleField(
+        "Select Field",
+        choices= [('0','All Users'), ('1','Admin')],
+        coerce=str
+    )
+    submit = SubmitField("Register")
+
 db = SQLAlchemy(app)
 
 class UserModel(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True) #"0 AI"
+    password_hash = db.Column(db.String(150), nullable=False) #"skdhfjdshfo"
+    groups = db.Column(db.String(150), nullable = False) #"0,1,2,3..."
     name = db.Column(db.String(150), nullable = False) #"Ola Normann"
     email = db.Column(db.String(150), nullable = False, unique = True) #"example@domain.cocaine"
-    password_hash = db.Column(db.String(150), nullable = False) #"skdhfjdshfo"
-    groups = db.Column(db.String(150), nullable = False) #"0,1,2,3..."
 
     def __init__(self, name, email, password_hash, groups):
         self.name = name
@@ -42,25 +81,23 @@ class UserModel(db.Model):
     
 
     @property
-    def groups(self):
-        return self.groups.split(",") #konverter til liste med gruppe tilhørighet
+    def group(self):
+        return self.groups
     
-    @groups.setter
-    def groups(self, value):
-        if isinstance(value, list):
-            self.groups = str(value).strip("[]")
-        elif isinstance(value, str):
-            self.groups = value
+    @group.setter
+    def group(self, value):
+        self.groups = str(value).strip("[]")
 
     @property
-    def password_hash(self):
-        return AttributeError("Password_hash is not retrievable")
+    def password(self):
+        return AttributeError("Password is not retrievable")
     
-    @password_hash.setter
-    def password_hash(self, password):
-        self.password_hash = generate_password_hash(password)
+    @password.setter
+    def password(self, password):
+        self._password_hash = generate_password_hash(password, 'sha256')
     
-    def verifypassword(self, password):
+    
+    def VerifyPassword(self, password):
         check_password_hash(self.password_hash, password)
         
 class GroupModel(db.Model):
@@ -114,6 +151,8 @@ def item(path,name):
                     #items.owner = owner_name
                     contents.append(items)
             return render_template('folder.html', contents = contents, current_folder = item)
+        case 1:
+            pass
 
 @app.route('/previous/<string:path>')
 def previous(path):
@@ -125,6 +164,27 @@ def previous(path):
         print(previous_path)
     return redirect(url_for('item', path = previous_path, name = path_list[-2:-1]))
     
+@app.route("/register", methods=['GET','POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = UserModel.query.filter_by(email = form.email.data).first()
+        if user is None:
+            hashed_pw = generate_password_hash(form.password.data, 'sha256')
+            group_str = str(form.groups.data).strip("[]")
+            user = UserModel(name = form.name.data,
+                email = form.email.data,
+                password_hash = hashed_pw,
+                groups = group_str
+            )
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('index'))
+        else:
+            flash('Email already exists', 'error')
+            return render_template('register.html', form=form)
+    print (form.errors)
+    return render_template("register.html", form=form)
             
 
 
